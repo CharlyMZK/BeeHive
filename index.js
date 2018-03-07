f2837c1082da1568742f10b82e3d13b168bcb19d
CharlyMZK <charlymrazeck@gmail.com>
2018-03-07 16:10:03 +0100

    threads

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var http = require('http');
 var https = require('https');
 var bodyParser = require('body-parser');
 var cors = require('cors');
-const spawn = require('threads').spawn;
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 
 var options = {
   key: fs.readFileSync('./certificats/ssl.key'),
@@ -18,23 +19,35 @@ app.use(cors());
 
 var dataRoute = require("./routes/data.js")
 
+if (cluster.isMaster) {
+    console.log(`Master ${process.pid} is running`);
+    app.use(bodyParser.json()); // support json encoded bodies
+    app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+    app.use('/data',dataRoute);
+    
+    var httpServer = http.createServer(app);
+    var httpsServer = https.createServer(options, app);
 
-const SerialPort = require('serialport');
-const Readline = SerialPort.parsers.Readline;
-const port = new SerialPort('/dev/ttyACM0');
-const parser = new Readline();
- 
-port.pipe(parser);
-parser.on('data', console.log);
-port.write('ROBOT PLEASE RESPOND\n');
+    httpServer.listen(8080);
+    httpsServer.listen(8443);
+    
+    // Fork workers.
+    for (let i = 0; i < 1; i++) {
+      cluster.fork();
+    }
+  
+    cluster.on('exit', (worker, code, signal) => {
+      console.log(`worker ${worker.process.pid} died`);
+    });
+  } else {
+    const SerialPort = require('serialport');
+    const Readline = SerialPort.parsers.Readline;
+    const port = new SerialPort('/dev/ttyACM0');
+    const parser = new Readline();
+    port.pipe(parser);
+    parser.on('data', console.log);
+    port.write('ROBOT PLEASE RESPOND\n');
+    console.log(`Worker ${process.pid} started`);
+  }
 
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-app.use('/data',dataRoute);
 
-var httpServer = http.createServer(app);
-var httpsServer = https.createServer(options, app);
-
-
-httpServer.listen(8080);
-httpsServer.listen(8443);
