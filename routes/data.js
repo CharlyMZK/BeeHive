b6aac840fce62871d47c0785376778aa1b0b6e20
Quentin <arcahyxe@gmail.com>
2018-03-08 14:31:15 +0100

    quickfix

--- routes/data.js
+++ routes/data.js
@@ -15,7 +15,7 @@ router.route('/')
                 return console.log(err);
             var lines = data.trim().split('\n');
             var lastLine = lines.slice(-1)[0];
-            res.send(lastLine.substring(1, lastLine.length-3));
+            res.send(lastLine);
         });
     })
     .post(function (req, res) {
